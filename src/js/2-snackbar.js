186361556e73dd7fef818144765ce87ad88fa0c5
mardaxrakys <163344559+mardaxrakys@users.noreply.github.com>
2025-04-13 08:25:29 +0300

    1win

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,56 +3,47 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('.form');
 
-form.addEventListener('submit', onSubmitForm);
-
-function onSubmitForm(event) {
+form.addEventListener('submit', event => {
   event.preventDefault();
-  const { delay, step, amount } = event.currentTarget.elements;
 
-  if (delay.value < 0 || step.value < 0 || amount.value < 0) {
-    iziToast.show({
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+  if (delay < 0) {
+    iziToast.warning({
       title: 'Warning',
-      message: `❗ Please enter a positive number`,
+      message: '❗ Please enter a positive delay',
       position: 'topCenter',
-      color: 'yellow',
     });
-  } else {
-    for (let i = 0; i < amount.value; i++) {
-      let position = i + 1;
-      const delays = Number(delay.value) + step.value * i;
-
-      createPromise(position, delays)
-        .then(({ position, delay }) => {
-          iziToast.show({
-            title: 'Success',
-            message: `✅ Fulfilled promise ${position} in ${delay}ms`,
-            position: 'topCenter',
-            color: 'green',
-          });
-        })
-        .catch(({ position, delay }) => {
-          iziToast.show({
-            title: 'Error',
-            message: `❌ Rejected promise ${position} in ${delay}ms`,
-            position: 'topCenter',
-            color: 'red',
-          });
-        });
-    }
+    return;
   }
 
-  event.currentTarget.reset();
-}
+  createPromise(delay, state)
+    .then(message => {
+      iziToast.success({
+        title: 'Success',
+        message,
+        position: 'topCenter',
+      });
+    })
+    .catch(error => {
+      iziToast.error({
+        title: 'Error',
+        message: error,
+        position: 'topCenter',
+      });
+    });
 
-function createPromise(position, delay) {
+  form.reset();
+});
+
+function createPromise(delay, state) {
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
-
     setTimeout(() => {
-      if (shouldResolve) {
-        resolve({ position, delay });
+      if (state === 'fulfilled') {
+        resolve(`✅ Fulfilled promise in ${delay}ms`);
       } else {
-        reject({ position, delay });
+        reject(`❌ Rejected promise in ${delay}ms`);
       }
     }, delay);
   });
